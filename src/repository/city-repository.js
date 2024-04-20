4e9b254b3a3a0e49e8ec2506b2456cef85548b87
Manglesh <mangleshyadav5456@gmail.com>
2024-04-20 14:31:36 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,6 +6,7 @@ class CityRepository {
             const city = await City.create({name});
             return city;
         } catch (error) {
+            console.log("Something went wrong in repo layer");
             throw {error};
         }
     }
@@ -17,7 +18,32 @@ class CityRepository {
                     id: cityId
                 }
             });
+            return true;
         } catch(error) {
+            console.log("Something went wrong in repo layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in repo layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            })
+        } catch (error) {
+            console.log("Something went wrong in repo layer");
             throw {error};
         }
     }
